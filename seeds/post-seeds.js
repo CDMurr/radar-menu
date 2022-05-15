2c496ebe5025b7245f5ab85535bd4bb61d9d0e8f
mhmunter <mark.munter1@gmail.com>
2022-05-15 16:06:00 -0500

    added to seeds file

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -4,17 +4,17 @@ const postdata = [
   {
     title: "Brisket.",
     comment_text: "The Brisket is the best",
-    user_id: 10,
+    user_id: 1,
   },
   {
     title: "Sides.",
     comment_text: "The Sides are the best!!!",
-    user_id: 8,
+    user_id: 2,
   },
   {
     title: "desserts.",
     comment_text: "The desserts are the best",
-    user_id: 1,
+    user_id: 3,
   },
 ];
 
